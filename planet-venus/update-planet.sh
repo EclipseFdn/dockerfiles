7a70dc8e3548868b7f5b1503db13d7f8ceeb8b70
Mikaël Barbero <mikael.barbero@gmail.com>
2019-01-25 18:02:03 +0100

    Added nginx and planet-venus dockerfiles

---
+++ planet-venus/update-planet.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+#*******************************************************************************
+# Copyright (c) 2019 Eclipse Foundation and others.
+# This program and the accompanying materials are made available
+# under the terms of the Eclipse Public License 2.0
+# which is available at http://www.eclipse.org/legal/epl-v20.html
+# SPDX-License-Identifier: EPL-2.0
+#*******************************************************************************
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+IFS=$'\n\t'
+
+PLANET="${1:-}"
+WWW="${2:-}"
+BRANCH_NAME="${3:-master}"
+
+cd "${PLANET}"
+while true; do
+  git fetch origin
+  git reset --hard origin/${BRANCH_NAME} 
+  planet planet.ini
+  if [[ -d "theme/css" ]]; then
+    cp -rf "theme/css" "${WWW}"
+  fi
+  if [[ -d "theme/authors" ]]; then 
+    cp -rf "theme/authors" "${WWW}"
+  fi
+
+  sleep 5
+done
