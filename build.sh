9c1b4b1934bef6161742bb5b08e798801567ca89
Mikaël Barbero <mikael.barbero@eclipse-foundation.org>
2020-08-19 15:30:52 +0200

    Added nginx for hugo (404) to CI

--- build.sh
+++ build.sh
@@ -17,6 +17,7 @@ SCRIPT_FOLDER="$(dirname "$(readlink -f "${0}")")"
 . "${SCRIPT_FOLDER}/build_init.sh"
 
 build nginx stable-alpine latest
+build nginx stable-alpine-for-hugo latest
 build nginx stable-alpine-for-staging
 
 build planet-venus debian-10-slim latest
