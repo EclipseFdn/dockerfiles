d3427690dbcd075de43fb1c55466cc6e5ee4fda4
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2023-11-09 18:48:49 +0100

    Assign latest tag to drupal-node d9.5.10-n18.18.2
    + clean up

--- build.sh
+++ build.sh
@@ -16,6 +16,8 @@ SCRIPT_FOLDER="$(dirname "$(readlink -f "${0}")")"
 
 . "${SCRIPT_FOLDER}/build_init.sh"
 
+# build_arg <image_name> <tag> [build-args] [latest]
+
 build nginx stable-alpine latest
 build nginx stable-alpine-for-staging
 
@@ -28,16 +30,12 @@ build hugo-node h0.99.1-n16.15.0
 build hugo-node h0.110.0-n18.13.0
 build hugo-node h0.76.5-n12.22.1 latest
 
-build drupal-node d9.5.10-n18.17.1 latest
-# build_arg <image_name> <tag> [build-args] [latest]
-build_arg drupal-node d9.5.10-n18.18.2 "--build-arg DRUPAL_VERSION=9.5.10 --build-arg NODE_VERSION=v18.18.2"
+build drupal-node d9.5.10-n18.17.1
+build_arg drupal-node d9.5.10-n18.18.2 "--build-arg DRUPAL_VERSION=9.5.10 --build-arg NODE_VERSION=v18.18.2" latest
 
 build stack-build-agent h79.1-n12.22.1-jdk11 latest
 build stack-build-agent h111.3-n18.17-jdk11
 build stack-build-agent h111.3-n18.17-jdk17
-
-#build java-api-base j11-openjdk latest
-#build java-api-base j17-openjdk
 
 build_arg java-api-base j11-openjdk "--build-arg JDK_VERSION=11:1.16-3" latest
 build_arg java-api-base j17-openjdk "--build-arg JDK_VERSION=17:1.16-3"
